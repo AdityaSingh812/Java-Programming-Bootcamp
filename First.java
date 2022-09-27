e4b80202908c52feaed275e310e2de47428146c2
Aditya Singh <111357751+AdityaSingh812@users.noreply.github.com>
2022-09-28 00:39:38 +0530

    Add files via upload
    First code of java

---
+++ First.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class First
+{
+public static void main(String[] st)
+{
+System.out.println("Welcome to the world of Java");
+
+System.out.print("Welcome to MSU");
+}
+}
