d35e09bad31d234a4c4cf5d8664bf165d4abe7ae
Aditya Singh <111357751+AdityaSingh812@users.noreply.github.com>
2022-09-28 00:41:53 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,9 @@
+public class Second
+{
+public static void main(String[] st)
+{
+System.out.println("Name-Aditya Singh");
+System.out.println("Address- 11 Shreenivas Makarpura");
+System.out.print("Contact no-7862000318");
+}
+}
