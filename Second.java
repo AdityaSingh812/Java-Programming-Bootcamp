70b58f60f6b55f6ecc83cdbbbefe0c003336a3fd
Aditya Singh <111357751+AdityaSingh812@users.noreply.github.com>
2022-09-28 00:45:32 +0530

    Update Second.java

--- Second.java
+++ Second.java
@@ -3,7 +3,7 @@ public class Second
 public static void main(String[] st)
 {
 System.out.println("Name-Aditya Singh");
-System.out.println("Address- 11 Shreenivas ");
+System.out.println("Address- 11 Shreenivas");
 System.out.print("Contact no-34556544432");
 }
 }
